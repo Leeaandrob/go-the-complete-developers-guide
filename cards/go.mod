ba792deb9f6bf97b525b60315552260dc9964601
Leandro Barbosa <leandro.barbosa@corp.globo.com>
2018-10-25 12:07:01 -0300

    leaning golang

--- cards/go.mod
+++ cards/go.mod
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 module cards
+=======
+module .
+>>>>>>> leaning golang
--- cards/main.go
+++ cards/main.go
@@ -1,7 +1,21 @@
 package main
 
+<<<<<<< HEAD
 func main() {
 	cards := newDeck()
 	cards.shuffle()
 	cards.print()
+=======
+import "fmt"
+
+func main() {
+	card := newCard()
+
+	fmt.Println(card)
+	printState()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+>>>>>>> leaning golang
 }
