a984fa0f35a507b41dc339b00b542640e41d94cf
Leandro Barbosa <leandrobar93@gmail.com>
2018-10-27 16:26:43 -0300

    refactoring the deck adding a method to return a instance of deck and use on main.go

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,21 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	// loop to append suit and value into cards(deck)
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // func receiver
 func (d deck) print() {
 	// lopping and print the el of the cards
--- cards/main.go
+++ cards/main.go
@@ -1,24 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 	//var card string = "Ace of Spades"
 
 	// creating a new slice
-	cards := deck{newCard(), newCard()}
-
-	// adding el to cards
-	cards = append(cards, newCard())
-	cards = append(cards, "Six of Spades")
-
-	// print the el of the cards
-	fmt.Println(cards)
+	cards := newDeck()
 
 	cards.print()
 
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
